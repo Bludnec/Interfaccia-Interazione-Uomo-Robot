d82567cea773bd45e190c91eeb7cf0a1b1a7b27f
Lorenzo <colavecchilorenzo@gmail.com>
2022-03-18 15:56:22 +0100

    Update sketch.js

--- p5Test/sketch.js
+++ p5Test/sketch.js
@@ -26,11 +26,22 @@ function Cell(i,j){
   this.i = i;
   this.j = j;
 
+  /* Posiziona le celle in ordine sullo schermo in base alle coordinate*/
   this.show = function(){
     var x = this.i*w;
     var y = this.j*w;
+
+    /* Creo le linee (up,right,left,down) intorno alle celle per creare i muri */
     stroke(255);
-    noFill();
-    rect(x,y,w,w);
+    line(x,y,x+w,y);
+    line(x+w,y,x+w,y+w);
+    line(x+w,y+w,x,y+w);
+    line(x,y+w,x,y);
+    //noFill();
+    //rect(x,y,w,w);
   }
+
 }
+
+
+  console.log(grid);
