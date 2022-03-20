037dacc4ad3918b35fa4805fe1b0bc704cb3673a
Lorenzo <colavecchilorenzo@gmail.com>
2022-03-20 12:55:21 +0100

    Update sketch.js

--- p5Test/sketch.js
+++ p5Test/sketch.js
@@ -4,12 +4,12 @@ var grid = [];
 
 function setup() {
   createCanvas(500, 500);
-  cols = floor(width/w);
-  rows = floor(height/w);
+  cols = floor(width / w);
+  rows = floor(height / w);
 
-  for(var j =0; j<rows;j++){
-    for(var i=0; i< cols; i++){
-      var cell = new Cell(i,j);
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
       grid.push(cell);
     }
   }
@@ -17,31 +17,41 @@ function setup() {
 
 function draw() {
   background(51);
-  for (var i = 0; i< grid.length;i++){
+  for (var i = 0; i < grid.length; i++) {
     grid[i].show();
   }
 }
 
-function Cell(i,j){
+function Cell(i, j) {
   this.i = i;
   this.j = j;
+  /* Boolean per i muri  [top,right,bottom,left] */
+  this.walls = [true, true, true, true];
+  /* Variabile per verificare se la cella è già stata visitata */
+  this.visited = false;
 
   /* Posiziona le celle in ordine sullo schermo in base alle coordinate*/
-  this.show = function(){
-    var x = this.i*w;
-    var y = this.j*w;
+  this.show = function () {
+    var x = this.i * w;
+    var y = this.j * w;
 
     /* Creo le linee (up,right,left,down) intorno alle celle per creare i muri */
     stroke(255);
-    line(x,y,x+w,y);
-    line(x+w,y,x+w,y+w);
-    line(x+w,y+w,x,y+w);
-    line(x,y+w,x,y);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
     //noFill();
     //rect(x,y,w,w);
-  }
-
+  };
 }
 
-
-  console.log(grid);
+console.log(grid);
