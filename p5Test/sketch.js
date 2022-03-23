482d0e8e71d61901c4771ca8586d51ea142381ad
Lorenzo <colavecchilorenzo@gmail.com>
2022-03-23 19:52:12 +0100

    Update sketch.js

--- p5Test/sketch.js
+++ p5Test/sketch.js
@@ -1,34 +1,53 @@
 var cols, rows;
 var w = document.getElementById("cell-size").value;
 
-var grid = [];
+var cellList = [];
 
 document.getElementById("drawButton").addEventListener("click", setup);
 
 function setup() {
-  grid = [];
+  cellList = [];
   w = document.getElementById("cell-size").value;
   rows = document.getElementById("height").value;
   cols = document.getElementById("width").value;
-  var canvas = createCanvas(cols * w, rows * w);
+  var canvas = createCanvas(cols * w + 400, rows * w);
   canvas.parent("canvas-zone");
-  canvas.mouseClicked(colorCell);
-  frameRate(5);
+  canvas.mouseClicked(clickedCell);
 
+  leftBuffer = createGraphics(cols * w, rows * w);
+  rightBuffer = createGraphics(400, rows * w);
+
+  // Draw on your buffers however you like
+  drawLeftBuffer();
+  drawRightBuffer();
+  // Paint the off-screen buffers onto the main canvas
+  image(leftBuffer, 0, 0);
+  image(rightBuffer, 400, 0);
+}
+
+function draw() {
+  console.log("draw");
+  frameRate(1);
+}
+
+function drawLeftBuffer() {
   /* Creazione della lista delle celle */
   for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols; i++) {
       var cell = new Cell(i, j);
-      grid.push(cell);
+      cellList.push(cell);
     }
+  }
+  for (var i = 0; i < cellList.length; i++) {
+    cellList[i].show();
   }
 }
 
-function draw() {
-  background(51);
-  for (var i = 0; i < grid.length; i++) {
-    grid[i].show();
-  }
+function drawRightBuffer() {
+  rightBuffer.background(255, 100, 255);
+  rightBuffer.fill(0, 0, 0);
+  rightBuffer.textSize(32);
+  rightBuffer.text("This is the right buffer!", 50, 50);
 }
 
 /* Funzione per ridisegnare il canvas dopo il click al bottone "Draw" */
@@ -46,20 +65,27 @@ function cellIndex(i, j) {
   return i + j * cols;
 }
 
-function Cell(i, j) {
-  this.i = i;
-  this.j = j;
-
-  this.zone = "";
-  /* Boolean per i muri  [top,right,bottom,left] */
-  this.walls = [true, true, true, true];
-  /* Variabile per verificare se la cella è già stata visitata */
-  this.visited = false;
-
+class Cell {
+  constructor(i, j) {
+    this.i = i;
+    this.j = j;
+    /* Zona della casa della cella */
+    this.zone = "";
+    /* Boolean per i muri  [top,right,bottom,left] */
+    this.walls = [true, true, true, true];
+    /* Variabile per verificare se la cella è già stata visitata */
+    this.visited = false;
+  }
   /* Posiziona le celle in ordine sullo schermo in base alle coordinate*/
-  this.show = function () {
+  show = function () {
     var x = this.i * w;
     var y = this.j * w;
+
+    /*Creo il rettangolo della cella */
+    fill(51);
+    noStroke();
+    rect(x, y, w, w);
+    noFill();
 
     /* Creo le linee (up,right,left,down) intorno alle celle per creare i muri */
     stroke(255);
@@ -75,16 +101,11 @@ function Cell(i, j) {
     if (this.walls[3]) {
       line(x, y + w, x, y);
     }
-    //noFill();
-    //rect(x,y,w,w);
   };
 }
 
-function colorCell() {
+function clickedCell() {
   var x = parseInt(mouseX / w);
   var y = parseInt(mouseY / w);
-  fill(255, 0, 255, 100);
-  rect(x * w, y * w, w, w);
-  var cell = grid[cellIndex(x, y)];
-  console.log(cell);
+  console.log(cellList[cellIndex(x, y)]);
 }
