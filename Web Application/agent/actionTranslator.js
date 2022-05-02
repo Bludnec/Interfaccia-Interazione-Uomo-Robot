5c5a916d511da0a1ff1b4e6de575cbc7bef9c40d
Lorenzo <colavecchilorenzo@gmail.com>
2022-05-02 19:04:58 +0200

    Update actionTranslator.js

--- Web Application/agent/actionTranslator.js
+++ Web Application/agent/actionTranslator.js
@@ -1,5 +1,29 @@
+/**
+ * Esempio: MOTION(Goal('to the living room')) & MANIPULATION(Entity(g9))
+ */
+
 function translator(formaPredicativaCorretta) {
+  /*Lista delle azioni da svolgere */
+  var actionList = [];
+
   if (formaPredicativaCorretta == null) {
     return null;
   }
+  /* Divido le varie azioni (&) */
+  var fpcSplit = formaPredicativaCorretta.split("&");
+  /* Rimuovo gli spazi dopo "&" */
+  for (var i = 0; i < fpcSplit.length; i++) {
+    fpcSplit[i] = fpcSplit[i].trim();
+  }
+
+  /* Salvo tutte le azioni in una lista */
+  for (var i = 0; i < fpcSplit.length; i++) {
+    var action = fpcSplit[i].substr(0, fpcSplit[i].indexOf("("));
+    actionList.push(action);
+  }
+
+  console.log(actionList);
+  console.log(fpcSplit);
 }
+
+translator("MOTION(Goal('to the living room')) & MANIPULATION(Entity(g9))");
