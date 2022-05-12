a836c20e4367a3725a0e881ad92727b3f1151207
Lorenzo <colavecchilorenzo@gmail.com>
2022-05-12 11:51:14 +0200

    Update app.py

--- Web Application/app.py
+++ Web Application/app.py
@@ -6,24 +6,3 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-
-
-# in JS
-# draw(){
-#   fetch('/list-objects', GET) ritorna list-objects
-# for each element in list-objects
-#   var el = new El(list-objets.getInt(id), list-objects.getString(name), ....)
-#   el.show()
-# }
-
-# FLASK (fa tipo da servlet)
-# def listObjects('/list-objects):
-#   list.json = chiamo dao-listObjects()
-#   return list.json
-
-# PYTHON + PYSWIP
-# dao-listObject(){
-#   chiede a prolog tramite PySwip
-#   retun list-objets 
-# }
