d365b03b092f4fb8f36ae9ef1665861996d0ffa0
Lorenzo <colavecchilorenzo@gmail.com>
2022-05-24 21:14:58 +0200

    Update allFetch.js

--- Web Application/static/js/allFetch.js
+++ Web Application/static/js/allFetch.js
@@ -2,6 +2,6 @@ function getAllElement() {
   var lista;
   fetch("/get-all-element")
     .then((response) => response.json())
-    .then((data) => (lista = data));
+    .then((data) => (lista = [...data]));
   return lista;
 }
