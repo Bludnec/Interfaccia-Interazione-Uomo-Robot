a9c7e3c4c34bebf0773b72d759f2b3c263bfe561
Lorenzo <colavecchilorenzo@gmail.com>
2022-06-18 19:14:27 +0200

    Update otherFunction.js

--- Web Application/static/js/otherFunction.js
+++ Web Application/static/js/otherFunction.js
@@ -5,18 +5,29 @@ var translateButton = document
 function lexicalMap() {
   var inputText = document.getElementById("input-text").value;
   var listInputText = inputText.split(" ");
+  var lexMapString = "";
 
   // data = [[id,List[...]], [id,List[...]], [id,List[...]]]
   getEntityLexRefOnMap().then((data) => {
     for (var i = 0; i < data.length; i++) {
       for (var j = 0; j < data[i][1]["List"].length; j++) {
-        console.log(data[i][1]["List"][j]);
         // controllo le relazioni tra lexical reference entità istanziate e parole
         // nell'input text
         for (var k = 0; k < listInputText.length; k++) {
-          console.log(listInputText[k]);
+          if (listInputText[k] == data[i][1]["List"][j]) {
+            lexMapString =
+              lexMapString +
+              data[i][1]["List"][0] +
+              "|" +
+              data[i][1]["List"][0].toUpperCase();
+          }
         }
       }
     }
+    console.log(lexMapString);
   });
 }
+
+/**
+ * DEVO ANCHE CAPIRE DOVE STANNO LE ZONE DELLA CASA PER INSERIRLE NELLA LEXICAL MAP
+ */
