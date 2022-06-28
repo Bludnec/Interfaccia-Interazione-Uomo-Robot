846b9831f9555a4dcd0b557a4766dff88ad69dde
Lorenzo <colavecchilorenzo@gmail.com>
2022-06-28 20:20:42 +0200

    Update lexicalMap.js

--- Web Application/static/js/lexicalMap.js
+++ Web Application/static/js/lexicalMap.js
@@ -14,8 +14,7 @@ function lexicalMap() {
   var counter = 0;
 
   getLexicalMap().then((data) => {
-    console.log(data);
-    console.log(positioningList);
+    console.log("data: ", data);
     listOfZone = [];
     for (var i = 0; i < data.length; i++) {
       if (data[i][1]["List"] != null) {
@@ -124,7 +123,15 @@ function lexicalMap() {
           var found = entityLexMap.find(
             (element) => element == positioningList[1][j][0]
           );
-          if (found != undefined) {
+          if (
+            found != undefined &&
+            !lexMapString.includes(
+              positioningList[1][j][0] +
+                " ON BOTTOM " +
+                positioningList[1][j][1] +
+                " | "
+            )
+          ) {
             lexMapString =
               lexMapString +
               positioningList[1][j][0] +
@@ -137,7 +144,15 @@ function lexicalMap() {
           var found = entityLexMap.find(
             (element) => element == positioningList[1][j][1]
           );
-          if (found != undefined) {
+          if (
+            found != undefined &&
+            !lexMapString.includes(
+              positioningList[1][j][0] +
+                " ON BOTTOM " +
+                positioningList[1][j][1] +
+                " | "
+            )
+          ) {
             lexMapString =
               lexMapString +
               positioningList[1][j][0] +
@@ -157,7 +172,16 @@ function lexicalMap() {
           var found = entityLexMap.find(
             (element) => element == positioningList[2][j][0]
           );
-          if (found != undefined) {
+          if (
+            found != undefined &&
+            !lexMapString.includes(
+              lexMapString +
+                positioningList[2][j][0] +
+                " INSIDE " +
+                positioningList[2][j][1] +
+                " | "
+            )
+          ) {
             lexMapString =
               lexMapString +
               positioningList[2][j][0] +
@@ -170,7 +194,16 @@ function lexicalMap() {
           var found = entityLexMap.find(
             (element) => element == positioningList[2][j][1]
           );
-          if (found != undefined) {
+          if (
+            (found != undefined) &
+            !lexMapString.includes(
+              lexMapString +
+                positioningList[2][j][0] +
+                " INSIDE " +
+                positioningList[2][j][1] +
+                " | "
+            )
+          ) {
             lexMapString =
               lexMapString +
               positioningList[2][j][0] +
@@ -184,8 +217,63 @@ function lexicalMap() {
     /**
      * Relazione tra entità: NEAR / FAR
      * Oggetti vicini se distanza < 2 celle
+     *
+     * Creo una lista (entityInfoList) di Entity in base agli id in entityLexMap
+     * per poi controllare la loro vicinanza
      */
-    console.log(entityLexMap);
+    var entityInfoList = [];
+
+    for (var i = 0; i < entityLexMap.length; i++) {
+      for (var j = 0; j < itemsList.length; j++) {
+        if (entityLexMap[i] == itemsList[j].id) {
+          entityInfoList.push(itemsList[j]);
+        }
+      }
+    }
+
+    console.log(positioningList.length);
+    // non funziona
+    for (var i = 0; i < entityInfoList.length; i++) {
+      for (var k = 0; k < positioningList[k].length; k++) {
+        console.log(
+          entityInfoList[i].id,
+          positioningList[k][0],
+          positioningList[k][1]
+        );
+        if (
+          entityInfoList[i].id == positioningList[k][0] ||
+          entityInfoList[i].id == positioningList[k][1]
+        ) {
+          console.log("splice");
+          entityInfoList.splice(i, 1);
+        }
+      }
+    }
+    // ----
+
+    console.log(entityInfoList);
+
+    for (var i = 0; i < entityInfoList.length; i++) {
+      for (var j = i + 1; j < entityInfoList.length; j++) {
+        var x1 = entityInfoList[i].position.x;
+        var x2 = entityInfoList[j].position.x;
+        var y1 = entityInfoList[i].position.y;
+        var y2 = entityInfoList[j].position.y;
+        var distanza = Math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        if (distanza < 2) {
+          lexMapString =
+            lexMapString +
+            entityInfoList[i].id +
+            " NEAR " +
+            entityInfoList[j].id +
+            " ";
+        }
+      }
+    }
+
+    if (lexMapString == " # ") {
+      lexMapString = "#NOMAP";
+    }
     console.log(lexMapString);
   });
 
